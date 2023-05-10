1f26380de364e25459361275d5e2e14a1285136b
Jonathan Richard <jonathanrichard947@gmail.com>
2023-05-10 12:35:02 -0300

    fix: unused memed imports

--- src/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as useMemed } from "./useMemed";
-export { default as useScriptLoader } from "./useScriptLoader";
-// import { useMemed, MemedProvider } from './useMemed'
-export { default as useSetupCommands } from "./useSetupCommands";
-export { default as useSetupPatient } from "./useSetupPatient";
-export { default as useActionButtonBind } from "./useActionButtonBind";
-
-// export { useMemed, MemedProvider }
