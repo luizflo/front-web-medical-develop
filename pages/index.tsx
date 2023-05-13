6a6dbf6a5d55ba49a2f2307148bfc4f5e636deb6
Jonathan Richard <jonathanrichard947@gmail.com>
2023-05-13 11:26:40 -0300

    fix: added homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import LoginPage from './patient/login'
-import Loading from './loading'
+import React from "react";
 import CreateAccount from "./patient/createAccount";
 
 export default function App() {
